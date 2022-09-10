a5aa6065716f8e72da383d58e48986819ea59fdb
Daniel <damul90@gmail.com>
2022-09-09 22:59:21 -0500

    Primer commit subiendo el programa 18.1

---
+++ 18.1/codigo.js
@@ -0,0 +1,16 @@
+
+/*
+Página de ejercicios de javascript: https://www.arkaitzgarro.com/javascript/capitulo-18.html
+
+18.1 Todo el código JavaScript se encuentre en un archivo externo llamado codigo.js y el script siga funcionando de la misma manera.
+Después del primer mensaje, se debe mostrar otro mensaje que diga "Soy el primer script"
+Añadir algunos comentarios que expliquen el funcionamiento del código
+Añadir en la página XHTML un mensaje de aviso para los navegadores que no tengan activado el soporte de JavaScript
+
+
+*/
+
+
+//alert("tu página tiene activado javascript");
+
+document.write("Soy el primer script");
