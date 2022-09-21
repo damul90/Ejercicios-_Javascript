84e310d91241e16d3e06d89afaf1581273edfa92
Daniel <damul90@gmail.com>
2022-09-21 18:24:47 -0500

    agrego el ejercicio 18.2

---
+++ 18.2/codigo.js
@@ -0,0 +1,6 @@
+
+/*
+Página de ejercicios de javascript: https://www.arkaitzgarro.com/javascript/capitulo-18.html
+*/
+var mensaje = "Hola mundo!\n que fácil es incluir 'comillas simples' y \"comillas dobles\"";
+alert(mensaje);
