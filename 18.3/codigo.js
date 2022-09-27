e3026690cc1e510913f6616c9efd99ccd1fc320d
Daniel <damul90@gmail.com>
2022-09-27 17:15:34 -0500

    Agrego el ejercicio 18.3 al repo

---
+++ 18.3/codigo.js
@@ -0,0 +1,11 @@
+
+/*
+Página de ejercicios de javascript: https://www.arkaitzgarro.com/javascript/capitulo-18.html
+
+Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por 
+pantalla los doce nombres utilizando la función console.log().
+
+*/
+
+let meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre",]
+console.log(meses);
